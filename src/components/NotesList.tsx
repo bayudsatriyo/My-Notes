8b245eef6a025d171119a4a95281d01f1a7edc45
bayudsatriyo <bayudsatriyo@gmail.com>
2024-01-06 00:03:59 +0700

    add conditional rendering empty notes

--- src/components/NotesList.tsx
+++ src/components/NotesList.tsx
@@ -14,37 +14,46 @@ function NotesList({
   onDeleteHandler,
   onArchiveHandler,
 }: NotesList) {
+  console.log(notes);
   return (
     <div className="notes__list w-full block bg-violet-900 mt-20 rounded-t-large">
       <h2 className="text-3xl font-bold text-center text-slate-50 py-10">
         Catatan
       </h2>
       <div className="flex flex-row gap-2 flex-wrap justify-center px-4">
-        {notes.map((note) => (
-          <div className="card-notes border-2 shadow-lg shadow-violet-500/50 bg-violet-700 border-violet-600 px-2 rounded-xl max-w-96 min-w-80 flex flex-col h-85 mb-5">
-            <NoteItems {...note} />
-            <ButtonNote
-              id={note.id}
-              onDeleteHandler={onDeleteHandler}
-              onArchiveHandler={onArchiveHandler}
-            />
-          </div>
-        ))}
+        {notes.length === 0 ? (
+          <h1 className="text-white pb-5">Notes Kosong</h1>
+        ) : (
+          notes.map((note) => (
+            <div className="card-notes border-2 shadow-lg shadow-violet-500/50 bg-violet-700 border-violet-600 px-2 rounded-xl max-w-96 min-w-80 flex flex-col h-85 mb-5">
+              <NoteItems {...note} />
+              <ButtonNote
+                id={note.id}
+                onDeleteHandler={onDeleteHandler}
+                onArchiveHandler={onArchiveHandler}
+              />
+            </div>
+          ))
+        )}
       </div>
       <h2 className="text-3xl font-bold text-center text-slate-50 py-10">
         Arsip
       </h2>
       <div className="flex flex-row gap-2 flex-wrap justify-center px-4">
-        {archive.map((arsip) => (
-          <div className="card-notes border-2 bg-violet-700 border-violet-600 px-2 rounded-xl max-w-96 min-w-80 flex flex-col h-85 shadow-lg shadow-violet-500/50 mb-5">
-            <NoteItems {...arsip} />
-            <ButtonArchive
-              id={arsip.id}
-              onDeleteHandler={onDeleteHandler}
-              onBackHandler={onArchiveHandler}
-            />
-          </div>
-        ))}
+        {archive.length === 0 ? (
+          <h1 className="text-white pb-5">Archive Kosong</h1>
+        ) : (
+          archive.map((arsip) => (
+            <div className="card-notes border-2 bg-violet-700 border-violet-600 px-2 rounded-xl max-w-96 min-w-80 flex flex-col h-85 shadow-lg shadow-violet-500/50 mb-5">
+              <NoteItems {...arsip} />
+              <ButtonArchive
+                id={arsip.id}
+                onDeleteHandler={onDeleteHandler}
+                onBackHandler={onArchiveHandler}
+              />
+            </div>
+          ))
+        )}
       </div>
     </div>
   );
